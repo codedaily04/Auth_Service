2c404a3f5e8da5538c84f4f0fbb5a23baf08ae2b
Abhishek Nishad <codedaily04@gmail.com>
2026-04-08 15:51:02 +0530

    Added Token create and verify with password checking

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,8 @@
+const jwt = require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+
 const UserRepository=require('../repository/user-repository');
+const {JWT_Key}=require('../config/serverConfig');
 
 class UserService{
     constructor(){
@@ -14,6 +18,36 @@ class UserService{
             throw error;
         }
     }
+    //Creating  a function for jwt tokens
+    createToken(user){
+        try {
+            const result=jwt.sign(user,JWT_Key,{expiresIn:'1d'});
+            return result;
+        } catch (error) {
+            console.log('Something is wrong is Token creation');
+            throw error;
+        }
+    }
+    //This function is used to verify the token which is created by the createToken function
+    verifyToken(token){
+        try {
+            const response=jwt.verify(token,JWT_Key);
+            return response;
+        } catch (error) {
+            console.log('Something is wrong is Token verification');
+            throw error;
+        }
+    }
+    //checking the password is correct or not
+    checkPassword(userInputPassword,encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPassword,encryptedPassword);
+        } catch (error) {
+            console.log('Something is wrong is password comparison');
+            throw error;
+        }
+    }   
+
 }
 
 module.exports=UserService;
